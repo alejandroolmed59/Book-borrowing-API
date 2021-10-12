6af8c45e48cf7cba08a6783793fa7b5e0ecb22e2
Jose Alejandro Olmedo <jose.olmedo@telusinternational.com>
2021-10-12 11:09:12 -0600

    Borrow unit test

--- src/test/java/com/olmedo/bookborrowing/BookBorrowingApplicationTests.java
+++ src/test/java/com/olmedo/bookborrowing/BookBorrowingApplicationTests.java
@@ -1,13 +1,41 @@
 package com.olmedo.bookborrowing;
 
+import com.olmedo.bookborrowing.controller.BorrowingController;
+import com.olmedo.bookborrowing.entity.Book;
+import com.olmedo.bookborrowing.entity.Borrowing;
+import com.olmedo.bookborrowing.entity.User;
+import com.olmedo.bookborrowing.service.BookService;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+
 
 @SpringBootTest
 class BookBorrowingApplicationTests {
 
-    @Test
-    void contextLoads() {
+
+    @Autowired
+    private BorrowingController borrowingController;
+
+   @Test
+    void contextLoads() throws Exception{
+        assertThat(borrowingController).isNotNull();
     }
+    @Test
+    void createBorrow() throws Exception {
+        Borrowing borrowing = new Borrowing();
+        Book book = new Book();
+        User user = new User();
+
+        book.setBookISBN("4862579499");
+        user.setUserId("00041017");
+        borrowing.setBookObj(book);
+        borrowing.setUserObj(user);
+        assertThat(borrowingController.create(borrowing));
+    }
+
 
 }
